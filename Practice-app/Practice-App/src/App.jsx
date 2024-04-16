98cc894aa088b536b9f788db90bf7e24f174bf9e
Glen357 <161297213+Glen357@users.noreply.github.com>
2024-04-16 15:54:21 +1200

    Added some notes to the Readme, and added comments to some of the files/components

--- Practice-app/Practice-App/src/App.jsx
+++ Practice-app/Practice-App/src/App.jsx
@@ -2,16 +2,19 @@
 import './App.css'
 // import React from 'react';
 import Button from './Components/Button';
+// import statement importing the button component I created
 
-
-
+// this const declares the the app then the handle click function
 const App = () => {
   const handleClick = () => {
     console.log('Button clicked!');
   };
+//  This is a return statement that contains what will be displayed on the browser, there is an h1(main title), a button component, that calls the button componet import
+//  and the onclick function it utilises Then there is a link to my GitHub Profile, followed by a div that has a className to define that portion of the code so that 
+// you can better style just that part with your css, this contains an image
 
   return (
-    <>
+    <>  
     <div>
       <h1>Code For Fun</h1>
       <Button onClick={handleClick} text="Press Me!" />
@@ -28,3 +31,4 @@ const App = () => {
 };
 
 export default App;
+// The export is a way of defining what the page is named so that it can be imported and called by the main jsx file
--- Practice-app/Practice-App/src/Components/Button.jsx
+++ Practice-app/Practice-App/src/Components/Button.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useState } from 'react'; 
+// the above import is a useState it is is a Hook that allows functional components to manage state by providing a simpler shorter syntax
 import PropTypes from 'prop-types';
+// the above import is a propType this is a way to specify the expected data type of each property passed to a component
 
+
+// this Const decleration is a button you can click on the browser. It is only simple, and has an initial state of being blue in colour
+//  The state is the original form (buttonColor 'blue') and the setState is what you are changing it to (seButtonColor 'Red')
 const Button = ({ onClick, text }) => {
   // State to track the button color
   const [buttonColor, setButtonColor] = useState('blue');
 
-  // Function to handle button click
+
+  // This Function is the piece of code that handles what happens when you click the button, which changes the button colour to red
+  
   const handleClick = () => {
-    // Change button color to red
+    // Change button colour to red
     setButtonColor('red');
-    // Call onClick function if provided
+    
     if (onClick) {
       onClick();
     }
@@ -22,6 +29,7 @@ const Button = ({ onClick, text }) => {
   );
 };
 
+// 
 Button.propTypes = {
   onClick: PropTypes.func,
   text: PropTypes.string.isRequired
